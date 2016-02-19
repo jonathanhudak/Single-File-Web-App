30f8cd6fff638a4e00c23904097b7666fd5f41b7
hudakdidit <jonathan@wintr.us>
2016-02-19 13:47:55 -0800

    Allow custom server port

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Single-File-Web-App
+# Single File Web App
+
+1. Install Node
+2. Run `npm install`
+3. Start express web server with `node index` or optionally pass a custom port `PORT=5000 node index`.
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var port = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
 
@@ -45,6 +46,6 @@ app.get('/about', function (req, res) {
   res.send(page(about));
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen( port, function () {
+  console.log(`Listening on port ${port}!`);
 });
